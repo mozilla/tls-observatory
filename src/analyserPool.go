b606654350ba635455f25c88eca9da16f6241cac
0xDiBa <bachtisd@gmail.com>
2014-10-20 22:32:38 +0300

    work in progress

--- src/analyserPool.go
+++ src/analyserPool.go
@@ -1,18 +1,112 @@
 package main
 
 import (
+	"crypto/sha1"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"log"
 	"runtime"
 	"sync"
-	"encoding/base64"
-	"crypto/x509"
-	"crypto/sha1"
 
-	"github.com/streadway/amqp"
 	"github.com/mattbaird/elastigo/api"
 	"github.com/mattbaird/elastigo/core"
+	"github.com/streadway/amqp"
 )
+
+var signatureAlgorithm = [...]string{
+	"UnknownSignatureAlgorithm",
+	"MD2WithRSA",
+	"MD5WithRSA",
+	"SHA1WithRSA",
+	"SHA256WithRSA",
+	"SHA384WithRSA",
+	"SHA512WithRSA",
+	"DSAWithSHA1",
+	"DSAWithSHA256",
+	"ECDSAWithSHA1",
+	"ECDSAWithSHA256",
+	"ECDSAWithSHA384",
+	"ECDSAWithSHA512",
+}
+
+var extKeyUsage = [...]string{
+	"ExtKeyUsageAny",
+	"ExtKeyUsageServerAuth",
+	"ExtKeyUsageClientAuth",
+	"ExtKeyUsageCodeSigning",
+	"ExtKeyUsageEmailProtection",
+	"ExtKeyUsageIPSECEndSystem",
+	"ExtKeyUsageIPSECTunnel",
+	"ExtKeyUsageIPSECUser",
+	"ExtKeyUsageTimeStamping",
+	"ExtKeyUsageOCSPSigning",
+	"ExtKeyUsageMicrosoftServerGatedCrypto",
+	"ExtKeyUsageNetscapeServerGatedCrypto",
+}
+
+var keyUsage = [...]string{
+	"KeyUsageDigitalSignature",
+	"KeyUsageContentCommitment",
+	"KeyUsageKeyEncipherment",
+	"KeyUsageDataEncipherment",
+	"KeyUsageKeyAgreement",
+	"KeyUsageCertSign",
+	"KeyUsageCRLSign",
+	"KeyUsageEncipherOnly",
+	"KeyUsageDecipherOnly",
+}
+
+var publicKeyAlgorithm = [...]string{
+	"UnknownPublicKeyAlgorithm",
+	"RSA",
+	"DAS",
+	"ECDSA",
+}
+
+type StoredCertificate struct {
+	Version                float64                  `json:"version"`
+	SignatureAlgorithm     string                   `json:"signatureAlgorithm"`
+	Issuer                 certIssuer               `json:"issuer"`
+	Validity               certValidity             `json:"validity"`
+	Subject                certSubject              `json:"subject"`
+	SubjectPublicKeyInfo   certSubjectPublicKeyInfo `json:"subjectPublicKeyInfo"`
+	X509v3Extensions       [][]string               `json:"x509v3Extensions"`
+	X509v3ExtendedKeyUsage [][]string               `json:"x509v3ExtendedKeyUsage"`
+	X509v3BasicConstraints string                   `json:"x509v3BasicConstraints"`
+	CA                     bool                     `json:"ca"`
+	Analysis               interface{}              `json:"analysis"`
+}
+
+type certIssuer struct {
+	Country      string `json:"c"`
+	Organisation string `json:"o"`
+	OrgUnit      string `json:"ou"`
+	CommonName   string `json:"cn"`
+}
+
+type certValidity struct {
+	NotBefore string `json:"notBefore"`
+	NotAfter  string `json:"notAfter"`
+}
+
+type certSubject struct {
+	Country          string `json:"c"`
+	Organisation     string `json:"o"`
+	OrgUnit          string `json:"ou"`
+	CommonName       string `json:"cn"`
+	BusinessCategory string `json:"businessCategory"`
+}
+
+type certSubjectPublicKeyInfo struct {
+	PublicKeyAlgorithm string `json:"publicKeyAlgorithm"`
+}
+
+type CertX509v3BasicConstraints struct {
+	CA       bool        `json:"ca"`
+	Analysis interface{} `json:"analysis"`
+}
 
 func failOnError(err error, msg string) {
 	if err != nil {
@@ -29,32 +123,52 @@ func SHA1Hash(data []byte) string {
 
 func panicIf(err error) {
 	if err != nil {
-		panic(fmt.Sprintf("%s",err))
+		panic(fmt.Sprintf("%s", err))
 	}
 }
 
-func worker(msgs <-chan amqp.Delivery){
+func worker(msgs <-chan amqp.Delivery) {
 
 	forever := make(chan bool)
 	defer wg.Done()
 
 	for d := range msgs {
 		var certif *x509.Certificate
-    	data, err := base64.StdEncoding.DecodeString(string(d.Body))
-    	panicIf(err)
+		data, err := base64.StdEncoding.DecodeString(string(d.Body))
+		panicIf(err)
 		certif, err = x509.ParseCertificate(data)
 		panicIf(err)
+
+		jsonCert, err := json.MarshalIndent(certtoStored(certif), "", "    ")
 		// Index a doc using Structs
-		_, err = core.Index("testindex", "user", SHA1Hash(certif.Raw), nil,`{"raw":"`+base64.StdEncoding.EncodeToString(certif.Raw)+`"}`)
+		_, err = core.Index("certificates", "certificate", SHA1Hash(certif.Raw), nil, jsonCert)
 		panicIf(err)
 		d.Ack(false)
 	}
-		
+
 	<-forever
 }
 
-var wg sync.WaitGroup
+func certtoStored(cert *x509.Certificate) StoredCertificate {
 
+	var stored = StoredCertificate{}
+
+	stored.Version = float64(cert.Version)
+	stored.SignatureAlgorithm = signatureAlgorithm[cert.SignatureAlgorithm]
+	stored.SubjectPublicKeyInfo.PublicKeyAlgorithm = publicKeyAlgorithm[cert.PublicKeyAlgorithm]
+	// stored.Issuer.Country = cert.Issuer.Country
+	// stored.Issuer.Organistion = cert.Issuer.Organisation
+	// stored.Issuer.OrgUnit = cert.Issuer.OrganizationalUnit
+	stored.Issuer.CommonName = cert.Issuer.CommonName
+	stored.Validity.NotBefore = cert.NotBefore.Local().String()
+	stored.Validity.NotAfter = cert.NotAfter.Local().String()
+	stored.Subject.CommonName = cert.Subject.CommonName
+
+	return stored
+
+}
+
+var wg sync.WaitGroup
 
 func main() {
 
@@ -70,11 +184,11 @@ func main() {
 
 	q, err := ch.QueueDeclare(
 		"scan_results_queue", // name
-		true,         // durable
-		false,        // delete when unused
-		false,        // exclusive
-		false,        // no-wait
-		nil,          // arguments
+		true,                 // durable
+		false,                // delete when unused
+		false,                // exclusive
+		false,                // no-wait
+		nil,                  // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
 
@@ -102,9 +216,9 @@ func main() {
 	runtime.GOMAXPROCS(cores)
 
 	for i := 0; i < cores; i++ {
-        wg.Add(1)
+		wg.Add(1)
 		go worker(msgs)
-    }
+	}
 
 	wg.Wait()
 }
