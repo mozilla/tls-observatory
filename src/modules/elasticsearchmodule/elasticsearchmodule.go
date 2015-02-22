0d6cc70f2965834f7c79bb30aa2ce71c724edb72
0xDiBa <bachtisd@gmail.com>
2015-02-22 13:14:05 +0200

    amqp/elasticsearch modules

---
+++ src/modules/elasticsearchmodule/elasticsearchmodule.go
@@ -0,0 +1,42 @@
+package elasticsearchmodule
+
+import (
+	elastigo "github.com/mattbaird/elastigo/lib"
+)
+
+var es *elastigo.Conn
+
+func RegisterConnection(URL string) {
+
+	es = elastigo.NewConn()
+	es.Domain = URL
+}
+
+func SearchbyID(index, doctype, id string) (elastigo.Hits, error) {
+
+	searchJson := `{
+	    "query" : {
+	        "term" : { "_id" : "` + id + `" }
+	    }
+		}`
+	res, err := es.Search(index, doctype, nil, searchJson)
+	return res.Hits, err
+}
+
+func SearchbyTerm(index, doctype, termname, termvalue string) (elastigo.Hits, error) {
+
+	searchJson := `{
+	    "query" : {
+	        "term" : { "` + termname + `" : "` + termvalue + `" }
+	    }
+		}`
+	res, err := es.Search(index, doctype, nil, searchJson)
+
+	return res.Hits, err
+}
+
+func Push(index, doctype, id string, data interface{}) error {
+	_, err := es.Index(index, doctype, id, nil, data)
+
+	return err
+}
