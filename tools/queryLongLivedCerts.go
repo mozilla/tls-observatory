8c8b25eba74d4e0141a9d33472473b5c5fdea1be
Julien Vehent <julien@linuxwall.info>
2015-04-23 13:26:41 -0400

    rework long lived cert query to scan all certs, not just mozilla's

--- tools/queryLongLivedCerts.go
+++ tools/queryLongLivedCerts.go
@@ -14,36 +14,49 @@ import (
 func main() {
 	es := elastigo.NewConn()
 	es.Domain = "localhost:9200"
-
-	filter := `{"query":{"regexp":{"x509v3Extensions.subjectAlternativeName":".+(mozilla|firefox|webmaker).+"}}, "from" : 0, "size" : 100000000}`
-	res, err := es.Search("observer", "certificate", nil, filter)
-	if err != nil {
-		panic(err)
-	}
-	seen := make(map[string]bool)
-	thirtyNineMonths := time.Duration(28512 * time.Hour)
-	for _, storedCert := range res.Hits.Hits {
-		cert := new(certificate.Certificate)
-		err = json.Unmarshal(*storedCert.Source, cert)
+	step := 0
+	for {
+		filter := fmt.Sprintf(`{"query":{"bool":{"must":[
+								{"match": {"validationInfo.Mozilla.isValid": "true"}},
+								{"match": {"ca": "false"}}
+								]}},
+			"from" : %d, "size" : %d}`, step, step+100)
+		res, err := es.Search("observer", "certificate", nil, filter)
 		if err != nil {
 			panic(err)
 		}
-		if _, ok := seen[cert.Hashes.SHA1]; ok {
-			// already processed, skip it
-			continue
+		if len(res.Hits.Hits) == 0 {
+			break
 		}
-		seen[cert.Hashes.SHA1] = true
-		na, err := time.Parse("2006-01-02 15:04:05 +0000 UTC", cert.Validity.NotAfter)
-		if err != nil {
-			panic(err)
+		seen := make(map[string]bool)
+		thirtyNineMonths := time.Duration(28512 * time.Hour)
+		for _, storedCert := range res.Hits.Hits {
+			cert := new(certificate.Certificate)
+			err = json.Unmarshal(*storedCert.Source, cert)
+			if err != nil {
+				panic(err)
+			}
+			if _, ok := seen[cert.Hashes.SHA1]; ok {
+				// already processed, skip it
+				continue
+			}
+			seen[cert.Hashes.SHA1] = true
+			na, err := time.Parse("2006-01-02 15:04:05 +0000 UTC", cert.Validity.NotAfter)
+			if err != nil {
+				panic(err)
+			}
+			nb, err := time.Parse("2006-01-02 15:04:05 +0000 UTC", cert.Validity.NotBefore)
+			if err != nil {
+				panic(err)
+			}
+			if na.Sub(nb) > thirtyNineMonths {
+				fmt.Printf("%s is valid from %s to %s (%.0f months) for domains %s, %s\n",
+					cert.Hashes.SHA1, cert.Validity.NotBefore, cert.Validity.NotAfter,
+					na.Sub(nb).Hours()/float64(24*30), cert.Subject.CommonName,
+					strings.Join(cert.X509v3Extensions.SubjectAlternativeName, ", "))
+			}
 		}
-		nb, err := time.Parse("2006-01-02 15:04:05 +0000 UTC", cert.Validity.NotBefore)
-		if err != nil {
-			panic(err)
-		}
-		if na.Sub(nb) > thirtyNineMonths {
-			fmt.Printf("%s   %s\t%s\n", cert.Hashes.SHA1, cert.Subject.CommonName, strings.Join(cert.X509v3Extensions.SubjectAlternativeName, " "))
-		}
+		step += 100
 	}
 }
 
