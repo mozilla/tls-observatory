160b1abbd87cc1cf0292d2460f55154cf83f1eb0
0xDiBa <bachtisd@gmail.com>
2015-09-25 01:57:20 +0300

    added draft certificate retrieval and storage funcs

--- src/modules/postgresmodule/certificate.go
+++ src/modules/postgresmodule/certificate.go
@@ -1,5 +1,7 @@
 package postgresmodule
 
+//using latest https://hub.docker.com/_/postgres/ image for testing
+
 import (
 	"certificate"
 )
@@ -15,7 +17,7 @@ func (db *DB) InsertCertificate(cert *certificate.Certificate) error {
 	first_seen, last_seen, is_ubuntu_valid, is_mozilla_valid, is_microsoft_valid, 
 	is_apple_valid, x509_basicConstraints, x509_crlDistPoints, x509_extendedKeyUsage
 	x509_authorityKeyIdentifier, x509_subjectKeyIdentifier, x509_keyUsage, x509_subjectAltName,
-	signature_algo, parent_id . raw_cert ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11
+	signature_algo, parent_id, raw_cert ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11
 	$12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24 )`,
 		cert.Hashes.SHA1, cert.Hashes.SHA256, cert.Issuer.CommonName, cert.Subject.CommonName,
 		cert.Version, cert.CA, cert.Validity.NotBefore, cert.Validity.NotAfter, cert.FirstSeenTimestamp,
@@ -31,7 +33,7 @@ func (db *DB) InsertCertificate(cert *certificate.Certificate) error {
 
 //certificate DB schema ( not finalised )
 //CREATE TABLE certificates  (
-//	id                         	serial NOT NULL,
+//	id                         	serial primary key,
 //	sha1_fingerprint           	bytea NOT NULL,
 //  sha256_fingerprint          bytea NOT NULL,
 //	serial_number              	varchar NULL,
@@ -63,28 +65,51 @@ func (db *DB) InsertCertificate(cert *certificate.Certificate) error {
 //	x509_nsCertType            	varchar NULL,
 //	x509_nsComment             	varchar NULL,
 //	x509_policyConstraints     	varchar NULL,
-//	x509_privateKeyUsagePeriod 	varchar NULL,
-//	x509_SMIME-CAPS            	varchar NULL,
 //	x509_issuerAltName         	varchar NULL,
-//	signature_algo             	openssl_nidln NULL,
+//	signature_algo             	varchar NULL,
 //  parent_id					numeric NULL,
 //	depth                      	int4 NULL,
 //	public_key_id              	int4 NULL,
-//	public_key_type            	openssl_nidln NULL,
+//	public_key_type            	varchar NULL,
 //	in_openssl_root_store      	bool NULL,
 //	in_mozilla_root_store      	bool NULL,
 //	in_windows_root_store      	bool NULL,
 //	in_apple_root_store        	bool NULL,
 //	is_revoked                 	bool NULL,
 //	revoked_at                 	timestamp NULL,
-//	reason_revoked             	crl_reason NULL,
 //	raw_cert					varchar NOT NULL
-//	PRIMARY KEY(id)
 //);
 
 func (db *DB) UpdateCertLastSeen(cert *certificate.Certificate) error {
 
 	_, err := db.Exec("UPDATE certificates SET last_seen=$1 WHERE sha1_fingerprint=$2", cert.LastSeenTimestamp, cert.Hashes.SHA1)
-
 	return err
+}
+
+func (db *DB) GetCertwithFingerprint(sha1 string) (*certificate.Certificate, error) {
+
+	row, err = db.QueryRow(`SELECT sha256_fingerprint,
+		issuer, subject, version, is_ca, valid_not_before, valid_not_after,
+		first_seen, last_seen, is_ubuntu_valid, is_mozilla_valid, is_microsoft_valid, 
+		is_apple_valid, x509_basicConstraints, x509_crlDistPoints, x509_extendedKeyUsage
+		x509_authorityKeyIdentifier, x509_subjectKeyIdentifier, x509_keyUsage, x509_subjectAltName,
+		signature_algo, parent_id, raw_cert
+		FROM certificates
+		WHERE sha1_fingerprint=$1`, sha1)
+
+	if err != nil {
+		return nil, err
+	}
+
+	cert := &certificate.Certificate{}
+
+	err = row.Scan(&cert.Hashes.SHA256, &cert.Issuer.CommonName, &cert.Subject.CommonName,
+		&cert.Version, &cert.CA, &cert.Validity.NotBefore, &cert.Validity.NotAfter, &cert.FirstSeenTimestamp,
+		&cert.LastSeenTimestamp, ubuntu_valid, mozilla_valid, msft_valid, apple_valid,
+		&cert.X509v3BasicConstraints, &cert.x509v3Extensions.CRLDistributionPoints,
+		&cert.x509v3Extensions.ExtendedKEyUsage, &cert.x509v3Extensions.AuthorityKeyId,
+		&cert.x509v3Extensions.SubjectKeyId, &cert.x509v3Extensions.KeyUsage,
+		&cert.x509v3Extensions.SubjectAlternativeName, &cert.SignatureAlgorithm,
+		&cert.ParentSignature)
+
 }
