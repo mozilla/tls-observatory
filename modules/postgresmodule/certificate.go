1067ae1aff41284b479aec2774ed5a51631023a5
0xDiBa <bachtisd@gmail.com>
2015-10-12 01:04:48 +0300

    removed schema comment

--- modules/postgresmodule/certificate.go
+++ modules/postgresmodule/certificate.go
@@ -31,48 +31,6 @@ func (db *DB) InsertCertificate(cert *certificate.Certificate) error {
 	return err
 }
 
-//certificate DB schema ( not finalised )
-//CREATE TABLE certificates  (
-//	id                         	serial primary key,
-//	sha1_fingerprint           	bytea NOT NULL,
-//  sha256_fingerprint          bytea NOT NULL,
-//	serial_number              	varchar NULL,
-//	version                    	integer NULL,
-//	subject                    	varchar NULL,
-//	issuer                     	varchar NULL,
-//	is_ca                      	bool NULL,
-//	is_self_signed             	bool NULL,
-//	not_valid_before           	timestamp NULL,
-//	not_valid_after            	timestamp NULL,
-//  first_seen					timestamp NULL,
-//  last_seen					timestamp NULL,
-//	is_valid                   	bool NULL,
-//	validation_error           	varchar NULL,
-//	is_ubuntu_valid           	bool NULL,
-//	is_mozilla_valid           	bool NULL,
-//	is_windows_valid           	bool NULL,
-//	is_apple_valid             	bool NULL,
-//	x509_basicConstraints      	varchar NULL,
-//	x509_crlDistributionPoints 	varchar NULL,
-//	x509_extendedKeyUsage      	varchar NULL,
-//	x509_authorityKeyIdentifier	varchar NULL,
-//	x509_subjectKeyIdentifier  	varchar NULL,
-//	x509_keyUsage              	varchar NULL,
-//	x509_certificatePolicies   	varchar NULL,
-//	x509_authorityInfoAccess   	varchar NULL,
-//	x509_subjectAltName        	varchar NULL,
-//	x509_issuerAltName         	varchar NULL,
-//	signature_algo             	varchar NULL,
-//  parent_id					numeric NULL,
-//	in_openssl_root_store      	bool NULL,
-//	in_mozilla_root_store      	bool NULL,
-//	in_windows_root_store      	bool NULL,
-//	in_apple_root_store        	bool NULL,
-//	is_revoked                 	bool NULL,
-//	revoked_at                 	timestamp NULL,
-//	raw_cert					varchar NOT NULL
-//);
-
 func (db *DB) UpdateCertLastSeen(cert *certificate.Certificate) error {
 
 	_, err := db.Exec("UPDATE certificates SET last_seen=$1 WHERE sha1_fingerprint=$2", cert.LastSeenTimestamp, cert.Hashes.SHA1)
