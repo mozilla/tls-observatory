b278fe1999cf5c9de88bdea16c0883fb8ff66e3c
0xDiBa <bachtisd@gmail.com>
2015-11-10 15:48:47 +0200

    changed scan struct

--- modules/postgresmodule/postgresmodule.go
+++ modules/postgresmodule/postgresmodule.go
@@ -3,10 +3,13 @@ package postgresmodule
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 	"time"
 
 	_ "github.com/lib/pq"
 )
+
+const separator = "--"
 
 //using latest https://hub.docker.com/_/postgres/ image for testing
 
@@ -15,17 +18,17 @@ type DB struct {
 }
 
 type Scan struct {
-	id               int64
-	time_stamp       time.Time
+	ID               int64
+	Timestamp        time.Time
 	Target           string
-	replay           int //hours or days
-	has_tls          bool
-	cert_id          int
-	trust_id         int
-	is_valid         bool
-	validation_error string
-	complperc        int
-	conn_info        []byte
+	Replay           int //hours or days
+	Has_tls          bool
+	Cert_id          int64
+	Trust_id         int64
+	Is_valid         bool
+	Validation_error string
+	Complperc        int
+	Conn_info        []byte
 }
 
 func RegisterConnection(dbname, user, password, hostport string, sslmode string) (*DB, error) {
@@ -46,39 +49,56 @@ func (db *DB) NewScan(domain string, rplay int) (Scan, error) {
 
 	timestamp := time.Now()
 
-	var ID int64
+	var id int64
 
-	err := db.QueryRow("INSERT INTO scans (timestamp,target,replay) VALUES($1,$2,$3) RETURNING id", timestamp, domain, rplay).Scan(&ID)
+	err := db.QueryRow("INSERT INTO scans (timestamp,target,replay,has_tls,is_valid,completion_perc,validation_error,conn_info) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING id", timestamp, domain, rplay, false, false, 0, "", []byte("null")).Scan(&id)
 
 	if err != nil {
 		return Scan{}, err
 	}
 
-	return Scan{id: ID, time_stamp: timestamp, replay: rplay}, nil
+	return Scan{ID: id, Timestamp: timestamp, Replay: rplay}, nil
 }
 
 func (db *DB) GetScan(id int64) (Scan, error) {
 
 	s := Scan{}
-	s.id = id
+	s.ID = id
 
-	row := db.QueryRow(`SELECT time_stamp, target, replay, has_tls, 	cert_id,
-	is_valid, completion_perc, validation_error, is_ubuntu_valid, is_mozilla_valid,
-	is_windows_valid, is_apple_valid, conn_info
-	FROM certificates WHERE id=$1`, id)
+	var cID, tID sql.NullInt64
 
-	err := row.Scan(&s.time_stamp, &s.Target, &s.replay, &s.has_tls, &s.cert_id,
-		&s.is_valid, &s.validation_error, &s.conn_info)
+	var isvalid sql.NullBool
 
-	if err != nil {
-		return s, err
+	row := db.QueryRow(`SELECT timestamp, target, replay, has_tls, cert_id, trust_id,
+	is_valid, completion_perc, validation_error, conn_info
+	FROM scans WHERE id=$1`, id)
+
+	err := row.Scan(&s.Timestamp, &s.Target, &s.Replay, &s.Has_tls, &cID, &tID,
+		&isvalid, &s.Complperc, &s.Validation_error, &s.Conn_info)
+
+	if cID.Valid {
+		s.Cert_id = cID.Int64
+	} else {
+		s.Cert_id = -1
 	}
 
-	return s, nil
+	if tID.Valid {
+		s.Trust_id = tID.Int64
+	} else {
+		s.Trust_id = -1
+	}
+
+	if isvalid.Valid {
+		s.Is_valid = isvalid.Bool
+	} else {
+		s.Is_valid = false
+	}
+
+	return s, err
 
 }
 
-func (db *DB) UpdateCompletionPercentage(id string, p int) error {
+func (db *DB) UpdateScanCompletionPercentage(id int64, p int) error {
 	_, err := db.Exec("UPDATE scans SET completion_perc=$1 WHERE id=$2", p, id)
 
 	return err
@@ -88,4 +108,26 @@ func (db *DB) InsertWorkerAnalysis(scanid int64, jsonRes []byte, workerName stri
 	_, err := db.Exec("INSERT INTO analysis(scan_id,worker_name,	output) VALUES($1,$2,$3)", scanid, workerName, jsonRes)
 
 	return err
+}
+
+func StringSliceToString(slice []string) string {
+
+	res := ""
+
+	for _, s := range slice {
+		if res == "" {
+			res = s
+		} else {
+			res = res + separator + s
+		}
+	}
+
+	return res
+
+}
+
+func StringToStringSlice(s string) []string {
+
+	return strings.Split(s, separator)
+
 }
