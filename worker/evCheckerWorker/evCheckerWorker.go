f0f24b265d463e324d1c7102b3cc539373d9df0a
Adrian Utrilla <adrianutrilla@gmail.com>
2016-11-18 17:30:51 +0100

    Encode output string as json

--- worker/evCheckerWorker/evCheckerWorker.go
+++ worker/evCheckerWorker/evCheckerWorker.go
@@ -91,6 +91,7 @@ func (w evWorker) Run(in worker.Input, res chan worker.Result) {
 		w.error(res, "Could not get output from ev-checker: %+v", err)
 		return
 	}
+	out, _ = json.Marshal(out)
 	res <- worker.Result{
 		Success:    true,
 		WorkerName: workerName,
