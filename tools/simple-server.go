6089dc5662cc561511262c020d9314c8edc41e25
Dimitris Bachtis <bachtisd@gmail.com>
2016-11-28 17:30:54 +0200

    add simple tls-server for local testing
    Added a simple RSA cert-key pair generated through crypto/tls's generate-cert script.

---
+++ tools/simple-server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"crypto/tls"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("This is our super secure simple server.\n"))
+	})
+	cfg := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+	}
+	srv := &http.Server{
+		Addr:         ":443",
+		Handler:      mux,
+		TLSConfig:    cfg,
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+	}
+	log.Fatal(srv.ListenAndServeTLS("/etc/cert.pem", "/etc/key.pem"))
+}
