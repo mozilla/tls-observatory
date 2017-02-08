d9eeeb4df54994071308e0416209a5f827ca6101
Julien Vehent <julien@linuxwall.info>
2017-02-08 12:23:34 -0500

    oops, forgot this file

---
+++ database/stats.go
@@ -0,0 +1,64 @@
+package database
+
+import "time"
+
+// CountPendingScans returns the total number of scans that are pending in the queue
+func (db *DB) CountPendingScans() (count int64, err error) {
+	err = db.QueryRow(`SELECT COUNT(*) FROM scans
+				  WHERE completion_perc = 0
+				  AND attempts < 3 AND ack = false`).Scan(&count)
+	return
+}
+
+// HourlyScansCount represents the number of scans completed over one hour
+type HourlyScansCount struct {
+	Hour  time.Time `json:"hour"`
+	Count int64     `json:"count"`
+}
+
+// CountLast24HoursScans returns a list of hourly scans count for the last 24 hours, sorted
+// from most recent the oldest
+func (db *DB) CountLast24HoursScans() (hourlyStats []HourlyScansCount, err error) {
+	rows, err := db.Query(`SELECT DATE_TRUNC('hour', "timestamp") AS hour, COUNT(*) 
+				     FROM scans
+				     WHERE timestamp > NOW() - INTERVAL '24 hours' AND ack=true AND completion_perc=100
+				     GROUP BY DATE_TRUNC('hour', "timestamp") ORDER BY 1 DESC`)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		var hsc HourlyScansCount
+		err = rows.Scan(&hsc.Hour, &hsc.Count)
+		if err != nil {
+			return
+		}
+		hourlyStats = append(hourlyStats, hsc)
+	}
+	return
+}
+
+// CountDistinctTargetsLast24Hours returns the number of unique targets scanned over the last 24 hours
+func (db *DB) CountDistinctTargetsLast24Hours() (count int64, err error) {
+	err = db.QueryRow(`SELECT COUNT(DISTINCT(target))
+				  FROM scans
+				  WHERE timestamp > NOW() - INTERVAL '24 hours'
+				  AND ack=true
+				  AND completion_perc=100`).Scan(&count)
+	return
+}
+
+// CountDistinctCertsSeenLast24Hours returns the count of unique certificates seen over the last 24 hours
+func (db *DB) CountDistinctCertsSeenLast24Hours() (count int64, err error) {
+	err = db.QueryRow(`SELECT COUNT(DISTINCT(id))
+				  FROM certificates
+				  WHERE last_seen > NOW() - INTERVAL '24 hours'`).Scan(&count)
+	return
+}
+
+// CountDistinctCertsAddedLast24Hours returns the count of unique certificates added over the last 24 hours
+func (db *DB) CountDistinctCertsAddedLast24Hours() (count int64, err error) {
+	err = db.QueryRow(`SELECT COUNT(DISTINCT(id))
+				  FROM certificates
+				  WHERE first_seen > NOW() - INTERVAL '24 hours'`).Scan(&count)
+	return
+}
