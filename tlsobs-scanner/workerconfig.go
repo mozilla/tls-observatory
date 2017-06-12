a65d96027f8ec4e2ed3bbc0f52d0ec81b707d417
Julien Vehent <julien@linuxwall.info>
2017-06-12 09:46:08 -0400

    Rename ciscoumbrella worker to top1m

--- tlsobs-scanner/workerconfig.go
+++ tlsobs-scanner/workerconfig.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	_ "github.com/mozilla/tls-observatory/worker/ciscoUmbrellaRank"
 	_ "github.com/mozilla/tls-observatory/worker/evCheckerWorker"
 	_ "github.com/mozilla/tls-observatory/worker/mozillaEvaluationWorker"
 	_ "github.com/mozilla/tls-observatory/worker/mozillaGradingWorker"
 	_ "github.com/mozilla/tls-observatory/worker/sslLabsClientSupport"
+	_ "github.com/mozilla/tls-observatory/worker/top1m"
 )
