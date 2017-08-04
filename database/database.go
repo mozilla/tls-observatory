60d67e456cb6da164de2b1ebdceeb9df23340d20
Adrian Utrilla <adrianutrilla@gmail.com>
2017-08-04 13:36:15 -0700

    Make scan attempts start at 0

--- database/database.go
+++ database/database.go
@@ -80,7 +80,7 @@ func (db *DB) NewScan(domain string, rplay int, jsonParams []byte) (Scan, error)
 			(timestamp, target, replay, has_tls, is_valid, completion_perc, validation_error, conn_info, ack, attempts, analysis_params)
 			VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
 			RETURNING id`,
-		timestamp, domain, rplay, false, false, 0, "", []byte("null"), false, 1, jsonParams).Scan(&id)
+		timestamp, domain, rplay, false, false, 0, "", []byte("null"), false, 0, jsonParams).Scan(&id)
 
 	if err != nil {
 		return Scan{}, err
