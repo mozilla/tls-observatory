4c6b4ddeded73680a7176711eda448963a600125
Julien Vehent <julien@linuxwall.info>
2018-02-27 07:19:49 -0500

    mozillaEvaluationWorker: don't use Sprintf when there are no formatting directives

--- worker/mozillaEvaluationWorker/mozillaEvaluationWorker.go
+++ worker/mozillaEvaluationWorker/mozillaEvaluationWorker.go
@@ -231,8 +231,7 @@ func isBad(c connection.Stored, cert certificate.Certificate) (bool, []string) {
 	}
 
 	if cert.SignatureAlgorithm == "UnknownSignatureAlgorithm" {
-		failures = append(failures,
-			fmt.Sprintf("certificate signature could not be determined, use a standard algorithm", cert.SignatureAlgorithm))
+		failures = append(failures, "certificate signature could not be determined, use a standard algorithm")
 		isBad = true
 	} else if _, ok := sigAlgTranslation[cert.SignatureAlgorithm]; !ok {
 		failures = append(failures,
