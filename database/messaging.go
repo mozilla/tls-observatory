9345fdace0378ebba79af2d62ae4eb8829ec5b4e
Grant Murphy <grant.murphy@elastic.co>
2018-03-08 14:33:32 +1000

    Rename package name Sirupsen to sirupsen
    Fixes #305

--- database/messaging.go
+++ database/messaging.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/Sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 	"github.com/lib/pq"
 
 	"github.com/mozilla/tls-observatory/logger"
--- worker/mozillaGradingWorker/keyexchangeGrading.go
+++ worker/mozillaGradingWorker/keyexchangeGrading.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/Sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 	"github.com/mozilla/tls-observatory/connection"
 	"github.com/mozilla/tls-observatory/constants"
 )
