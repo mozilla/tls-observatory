e3ec9179f6b33a1586fe62d0a06fd190bd82ddca
Julien Vehent <julien@linuxwall.info>
2018-05-08 14:12:56 -0400

    Use {} instead of [] as default DB value for mozillaPolicyV2_5

--- database/migrations/1.3.3.sql
+++ database/migrations/1.3.3.sql
@@ -2,4 +2,4 @@ ALTER TABLE certificates ADD COLUMN x509_extendedKeyUsageOID jsonb NULL;
 UPDATE certificates SET x509_extendedkeyusageoid = '[]'::jsonb WHERE x509_extendedkeyusageoid IS NULL;
 
 ALTER TABLE certificates ADD COLUMN mozillaPolicyV2_5 jsonb NULL;
-UPDATE certificates SET mozillaPolicyV2_5 = '[]'::jsonb WHERE mozillaPolicyV2_5 IS NULL;
+UPDATE certificates SET mozillaPolicyV2_5 = '{}'::jsonb WHERE mozillaPolicyV2_5 IS NULL;
